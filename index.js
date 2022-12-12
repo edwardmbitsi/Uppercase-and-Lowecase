71c74a15c338a647b9cfde03f3bdd1ce221f29c6
Edward Mbitsi <93251536+edwardmbitsi@users.noreply.github.com>
2022-12-13 00:33:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let text = prompt("Enter A Phrase");
 let result = text.toLowerCase();
+console.log(result);
 
 
-let text = prompt("Enter Another Phrase");
-let result = text.toUpperCase();
+let text1 = prompt("Enter Another Phrase");
+let result1 = text1.toUpperCase();
+console.log(result1);
